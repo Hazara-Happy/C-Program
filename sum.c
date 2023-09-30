49ee1019d883d05cecc37b6b46a242d0a82e2181
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-09-30 17:23:34 +0600

    The sum of two number by c_program

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+
+int number1=10,number2=20;
+printf("Enter two numbers: ");
+scanf("%d%d",&number1,&number2);
+int sum;
+sum = number1+number2;
+printf(" The value of sum is %d",sum);
+
+
+   return 0;
+
+
+}
