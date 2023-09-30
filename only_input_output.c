0ffe5fc5e9da7d8fcdabc41ee9ea8e42be0bda9c
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-09-30 09:41:00 +0600

    only input output by c-program

---
+++ only_input_output.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main ()
+
+{
+    int a,b;
+
+    printf("Enter two numbers: ");
+
+    scanf("%d%d",&a,&b);
+    printf("The value of a number is %d\n",a);
+    printf("The value of b number is %d",b);
+
+
+
+
+
+
+
+
+    return 0;
+}
