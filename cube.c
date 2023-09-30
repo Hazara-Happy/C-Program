e8daa6d9d68cfc3107ba64f7020422fe75ab7bc4
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-09-30 23:39:12 +0600

    cube(n*n*n) by c-program

---
+++ cube.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    double n;
+
+
+    printf("Enter the value of n");
+    scanf("%lf", &n);
+
+
+    double cube = (n * n * n);
+
+
+    printf("%lf",cube);
+
+    return 0;
+    }
