bc8ac96a2a648a6ba4471fd0d765f4d52d2e59e1
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-10-03 12:38:37 +0600

    The sum of ASCII value by c-program

---
+++ Ascii_value.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    char x='A',y='B';
+
+
+
+
+    printf("Enter two characters: ");
+    scanf(" %c%c",&A,&B);
+
+    int sum=0;
+    sum=A+B;
+
+    // Display the ASCII value
+    printf("The ASCII value of %d",sum);
+
+    return 0;
+}
