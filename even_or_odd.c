a6944d275bb0e017379ab0ad63f6f4de1ddb3d01
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-10-07 23:32:09 +0600

    Add files via upload

---
+++ even_or_odd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+printf("enter the input: ");
+
+scanf("%d",&x);
+
+if(x%2==0)
+
+{
+    printf("the input is an even number./n");
+
+}
+
+ else
+{
+    printf("the input is an odd number./n");
+
+}
+
+return 0;
+}
+
+
