2167edb4015585a94a946a28ca98160139a0c6c3
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-10-07 23:30:34 +0600

    area of triangle by c-program

---
+++ area_of_triangle.c
@@ -0,0 +1,38 @@
+
+#include<stdio.h>
+int main()
+
+{
+   float height,base;
+
+
+     printf("enter the height: ");
+ scanf("%f",&height);
+ printf("enter the base: ");
+ scanf("%f",&base);
+
+
+ double area;
+ area=(height*base)/2;
+ //print the area of triangle
+
+ printf("the area of triangle is%lf",area);
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
