591d3503ad6ec2fc173a7cf93fa967b0fcd39d0e
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-10-07 23:34:06 +0600

    Add files via upload

---
+++ x is large or y is large.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main ()
+{
+
+int x,y;
+printf("enter two inputs: ");
+scanf("%d%d",&x,&y);
+
+if (x>y)
+{
+    printf("x is large");
+
+}
+else if(y>x)
+{
+    printf("y is large");
+
+}
+
+  else
+  {
+      printf("they are equal");
+
+  }
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+}
