7d3767a138214c9ee90c8e2d6d98f4411916cb2e
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-11-16 21:09:35 +0600

    basic array

---
+++ Array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+
+
+int ara[6];
+int i;
+
+ for(i=0;i<6;i++)
+ {
+   scanf("%d",&ara[i]);
+
+
+ }
+ for(i=0;i<6;i++)
+ {
+     printf("%d\n",ara[i]);
+ }
+
+ return 0;
+}
+
+
