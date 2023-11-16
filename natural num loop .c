3b8a5c56a3598ecf30a89a87084ebf50c8cfbb4b
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-11-16 21:04:10 +0600

    for loo by c-program

---
+++ natural num loop .c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+
+
+    scanf("%d", &n);
+
+printf("enter any number 1 to 10  %d",n);
+
+
+    for(i=1; i<=n; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
