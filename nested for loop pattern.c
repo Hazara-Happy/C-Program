4c7bd75add32944933500d1ab756a9a2c6128f64
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-11-26 20:14:43 +0600

    Add files via upload

---
+++ nested for loop pattern.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for(i=1;i<=6;i++)
+    {
+        //print 8 star
+        printf("%d line",i);
+
+for(j=1;j<=8;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
