c78b65f5b6f414444028489d08f150da904a3456
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-11-26 20:08:09 +0600

    counting length

---
+++ more than 10 character words should be replaced abbreviate .c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<string.h>
+int main ()
+
+{ char name[100];
+gets(name);
+int l=strlen(name)-1;
+if(l>10)
+{
+    //abbreviate
+    printf("%c%d%c",name[0],l -2,name[l-1]);
+}
+else
+{
+    puts(name);
+}
+
+return 0 ;
+}
+
