5536b81afb944814b9e5cfb988b2b7af121586fb
Hazara Akter Happy <146458391+Hazara-Happy@users.noreply.github.com>
2023-11-26 20:17:01 +0600

    nested loop

---
+++ team.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,i,a,counter,j;
+    scanf("%d", &n);
+    int solved=0;
+
+    for(i=0;i<n;i++)
+    {
+
+        counter=0;
+
+    for(j=0;j<5;j++)
+    {
+       scanf("%d",&a);
+       if(a==1)
+        counter++;
+    }
+    if(counter>=2)
+        solved++;
+
+    }
+
+    printf("%d\n",solved);
+
+
+
+
+    return 0;
+}
